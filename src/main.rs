cf10efc6f4efbfcd3bd8fb2de547b79b2da65300
thisdotrob <this.rob@protonmail.com>
2024-04-09 08:11:46 +0100

    Add camera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod camera;
 mod debug;
 mod movement;
 mod spaceship;
 
 use bevy::prelude::*;
+use camera::CameraPlugin;
 use debug::DebugPlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
@@ -10,8 +12,9 @@ use spaceship::SpaceshipPlugin;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(SpaceshipPlugin)
-        .add_plugins(MovementPlugin)
+        .add_plugins(CameraPlugin)
         .add_plugins(DebugPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(SpaceshipPlugin)
         .run();
 }
