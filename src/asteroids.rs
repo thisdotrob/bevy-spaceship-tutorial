ff01675a22b1434884483237cac132114684fc3b
Rob Stevenson <this.rob@protonmail.com>
2024-04-09 20:25:00 +0100

    Add AsteroidsPlugin

---
+++ src/asteroids.rs
@@ -0,0 +1,59 @@
+use std::ops::Range;
+
+use bevy::prelude::*;
+use rand::Rng;
+
+use crate::movement::{Acceleration, Velocity, MovingObjectBundle};
+
+const SPAWN_RANGE_X: Range<f32> = -25.0..25.0;
+const SPAWN_RANGE_Z: Range<f32> = 0.0..25.0;
+const SPAWN_TIME_SECONDS: f32 = 1.0;
+
+#[derive(Component, Debug)]
+pub struct Asteroid;
+
+#[derive(Resource, Debug)]
+pub struct SpawnTimer {
+    timer: Timer,
+}
+
+pub struct AsteroidPlugin;
+
+impl Plugin for AsteroidPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(SpawnTimer {
+            timer: Timer::from_seconds(SPAWN_TIME_SECONDS, TimerMode::Repeating),
+        }).add_systems(Update, spawn_asteroid);
+    }
+}
+
+fn spawn_asteroid(mut commands: Commands, mut spawn_timer: ResMut<SpawnTimer>, time: Res<Time>, asset_server: Res<AssetServer>) {
+    spawn_timer.timer.tick(time.delta());
+    if !spawn_timer.timer.just_finished() {
+        return;
+    }
+
+    let mut rng = rand::thread_rng();
+
+    let translation = Vec3::new(
+        rng.gen_range(SPAWN_RANGE_X),
+        0.,
+        rng.gen_range(SPAWN_RANGE_Z),
+    );
+
+    let mut random_unit_vector =
+        || Vec3::new(rng.gen_range(-1.0..1.0), 0., rng.gen_range(-1.0..1.0)).normalize_or_zero();
+
+    let velocity = random_unit_vector();
+    let acceleration = random_unit_vector();
+
+    commands.spawn((MovingObjectBundle {
+        velocity: Velocity::new(velocity),
+        acceleration: Acceleration::new(acceleration),
+        model: SceneBundle {
+            scene: asset_server.load("Enemy Flying.glb#Scene0"),
+            transform: Transform::from_translation(translation),
+            ..default()
+        },
+    }, Asteroid));
+}
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod camera;
 mod debug;
 mod movement;
 mod spaceship;
+mod asteroids;
 
 use bevy::prelude::*;
 use camera::CameraPlugin;
 use debug::DebugPlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
+use asteroids::AsteroidPlugin;
 
 fn main() {
     App::new()
@@ -21,5 +23,6 @@ fn main() {
         .add_plugins(DebugPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(SpaceshipPlugin)
+        .add_plugins(AsteroidPlugin)
         .run();
 }
