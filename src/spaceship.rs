1e6cb9f69974f517c3061ab12caa75481094b3da
Rob Stevenson <this.rob@protonmail.com>
2024-04-09 20:04:41 +0100

    Add Acceleration

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::movement::{MovingObjectBundle, Velocity};
+use crate::movement::{Acceleration, MovingObjectBundle, Velocity};
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
 const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
@@ -16,9 +16,8 @@ impl Plugin for SpaceshipPlugin {
 fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(
         MovingObjectBundle {
-            velocity: Velocity {
-                value: STARTING_VELOCITY,
-            },
+            velocity: Velocity::new(STARTING_VELOCITY),
+            acceleration: Acceleration::new(Vec3::ZERO),
             model: SceneBundle {
                 scene: asset_server.load("Spaceship.glb#Scene0"),
                 transform: Transform::from_translation(STARTING_TRANSLATION),
